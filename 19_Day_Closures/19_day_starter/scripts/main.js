af1c7fa457bc4c96104508f6f99d878ccfbb2076
Sebfraagodt <sebastian@fraagodt.no>
2024-11-18 10:57:34 +0100

    day 19

--- 19_Day_Closures/19_day_starter/scripts/main.js
+++ 19_Day_Closures/19_day_starter/scripts/main.js
@@ -1,2 +1,43 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+function outerFunction1() {
+    let count = 0;
+    function innerFunction() {
+        count++;
+        return count;
+    }
+
+    return innerFunction;
+}
+const innerFunc = outerFunction1();
+
+console.log(innerFunc());
+console.log(innerFunc());
+console.log(innerFunc());
+
+console.log('\n');
+
+function outerFunction2() {
+    let count = 0;
+    function plusOne() {
+        count++;
+        return count;
+    }
+    function minusOne() {
+        count--;
+        return count;
+    }
+    function plusTwo() {
+        count += 2;
+        return count;
+    }
+
+    return {
+        plusOne: plusOne(),
+        minusOne: minusOne(),
+        plusTwo: plusTwo(),
+    };
+}
+const innerFuncs = outerFunction2();
+
+console.log(innerFuncs.plusOne);
+console.log(innerFuncs.minusOne);
+console.log(innerFuncs.plusTwo);
